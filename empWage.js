e6e269e1b6e5da5bd317e9894019b28136f66526
Komal-8080 <shindeg163@gmail.com>
2021-02-09 17:58:14 +0530

    REFACTOR: Calculating wages for a month

--- empWage.js
+++ empWage.js
@@ -4,6 +4,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
        switch (empCheck) {
@@ -17,9 +18,13 @@ function getWorkingHours(empCheck) {
                	        return 0;
         }
 }
-let empCheck = Math.floor(Math.random() *10) %3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Hours: "+empHrs +" Emp Wage: "+empWage);
+
+let totalEmpHrs = 0;
+for ( let day =0; day < NUM_OF_WORKING_DAYS; day++) {
+	let empCheck = Math.floor(Math.random() *10) %3;
+	totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Totla Hours: "+totalEmpHrs +" Emp Wage: "+empWage);
 
 

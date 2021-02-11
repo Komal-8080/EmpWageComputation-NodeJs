e0287ee4f0bf23db0b351d04b9d64e101c912c50
Komal-8080 <shindeg163@gmail.com>
2021-02-11 10:44:22 +0530

    ADDED:  Get Total Hours and Total Wage for Month Using Objects

--- empWage.js
+++ empWage.js
@@ -46,3 +46,12 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 
 //UC10 - Objects
 console.log("UC10 Showing Daily hours worked and wage earned: "+empDailyHrsAndWageArr);
+
+//UC 11A to UC 11D using object functions along with arrow functiions
+let totalWages = empDailyHrsAndWageArr
+		.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage >0)
+		.reduce((totalWage, dailyHrsAndWage) => totalWage += dailyHrsAndWage.dailyWage, 0);
+let totalHours = empDailyHrsAndWageArr
+		.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+		.reduce((totalHours, dailyHrsAndWage) => totalHours += dailyHrsAndWage.dailyHours, 0);
+console.log("UC 11A total hours: "+totalHours+ "Total Wages: "+totalWages);
